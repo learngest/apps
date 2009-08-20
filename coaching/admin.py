f1922d4f0be56e59ce3353c39f1daa1644549599
JcB <jcbagneris@gmail.com>
2009-08-20 17:24:10 +0200

    Username = email

--- coaching/admin.py
+++ coaching/admin.py
@@ -30,6 +30,7 @@ class GroupeAdmin(admin.ModelAdmin):
 admin.site.register(Groupe, GroupeAdmin)
 
 class UtilisateurAdminForm(ModelForm):
+    username = forms.EmailField(label=_("Username"), max_length=75)
     password1 = forms.CharField(label=_("Password"), widget=forms.PasswordInput,
             required=False)
     password2 = forms.CharField(label=_("Password confirmation"), 
@@ -37,7 +38,7 @@ class UtilisateurAdminForm(ModelForm):
 
     class Meta:
         model = Utilisateur
-        exclude = ('password',)
+        exclude = ('password', 'email')
 
     def clean_password2(self):
         password1 = self.cleaned_data.get("password1", "")
@@ -48,17 +49,14 @@ class UtilisateurAdminForm(ModelForm):
 
     def save(self, commit=True):
         user = super(UtilisateurAdminForm, self).save(commit=False)
+        user.email = user.username
         if self.cleaned_data["password1"]:
             user.set_password(self.cleaned_data["password1"])
         if commit:
             user.save()
         return user
 
-class UtilisateurClassicAdminForm(ModelForm):
-    class Meta:
-        model = Utilisateur
-
-class UtilisateurCreationForm(ModelForm):
+class UtilisateurClassicCreationForm(ModelForm):
     username = forms.RegexField(label=_("Username"), max_length=30, regex=r'^\w+$',
         help_text = _("Required. 30 characters or fewer. Alphanumeric characters only (letters, digits and underscores)."),
         error_message = _("This value must contain only letters, numbers and underscores."))
@@ -91,15 +89,49 @@ class UtilisateurCreationForm(ModelForm):
             user.save()
         return user
 
+class UtilisateurCreationForm(ModelForm):
+    username = forms.EmailField(label=_("Username"), max_length=75)
+    password1 = forms.CharField(label=_("Password"), widget=forms.PasswordInput)
+    password2 = forms.CharField(label=_("Password confirmation"), widget=forms.PasswordInput)
+
+    class Meta:
+        model = Utilisateur
+        fields = ("username",)
+
+    def clean_username(self):
+        username = self.cleaned_data["username"].lower()
+        try:
+            Utilisateur.objects.get(username=username)
+        except Utilisateur.DoesNotExist:
+            return username
+        raise forms.ValidationError(_("A user with that username already exists."))
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1", "")
+        password2 = self.cleaned_data["password2"]
+        if password1 != password2:
+            raise forms.ValidationError(_("The two password fields didn't match."))
+        return password2
+
+    def save(self, commit=True):
+        user = super(UtilisateurCreationForm, self).save(commit=False)
+        user.email = user.username
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
 class UtilisateurAdmin(UserAdmin):
     form = UtilisateurAdminForm
     add_form = UtilisateurCreationForm
     fieldsets = (
-        #(None, {'fields': ('username', 'password')}),
         (None, {'fields': ('username', 'password1', 'password2')}),
         (_(u"Informations personnelles"), 
-                {'fields': ('last_name', 'first_name', 'email')}),
+                {'fields': ('last_name', 'first_name')}),
         (_(u"Paramètres"), {'fields': ('langue', 'groupe', 'fermeture')}),
+        (_('Permissions'), {'fields': ('is_staff', 'is_active', 'is_superuser', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        (_('Groups'), {'fields': ('groups',)}),
     )
     list_display = ('groupe', 'username', 'last_name', 'first_name')
     list_display_links = ('username',)
